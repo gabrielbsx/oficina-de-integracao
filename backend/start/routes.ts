7e4e335986eb35ea0234d1a810dbbaf1fb3f5a73
Gabriel <gabrielturing@gmail.com>
2022-09-03 00:37:54 -0300

    chore: pass test to root path api

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -1,5 +1,6 @@
 import Route from '@ioc:Adonis/Core/Route'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-Route.get('/', async () => {
-  return 'Projeto de Oficina de Integração 2 - API'
+Route.get('/', async ({ response }: HttpContextContract) => {
+  return response.send('Projeto de Oficina de Integração 2 - API')
 })
--- backend/tests/functional/hello_world.spec.ts
+++ backend/tests/functional/hello_world.spec.ts
@@ -4,5 +4,5 @@ test('display welcome page', async ({ client }) => {
   const response = await client.get('/')
 
   response.assertStatus(200)
-  response.assertBodyContains({ hello: 'world' })
+  response.assertTextIncludes('Projeto de Oficina de Integração 2 - API')
 })
