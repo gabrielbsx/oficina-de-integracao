0e556e076f688d9a0a480417ea9b0455beac6415
Gabriel <gabrielturing@gmail.com>
2022-09-21 12:22:56 -0300

    feat: make a cliente model

---
+++ backend/app/Models/Cliente.ts
@@ -0,0 +1,28 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Cliente extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public nome: string
+
+  @column()
+  public email: string
+
+  @column()
+  public cpf: string
+
+  @column()
+  public dataNascimento: Date
+
+  @column()
+  public senha: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
