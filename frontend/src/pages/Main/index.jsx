5d488c7571f795c586bb68cdcd0578f128f2b228
Viniciusslara <vinicius.lara2017@hotmail.com>
2022-09-22 21:13:06 -0300

    chore: create home page and refactor the frontend structure

---
+++ frontend/src/pages/Main/index.jsx
@@ -0,0 +1,22 @@
+
+import Header from "../../components/Header"
+import './index.css'
+import logo from '../../assets/logo.svg'
+
+const Main = () =>{
+    return(
+        <div>
+            <Header screem="Main" />
+
+            <div className="logo">
+                <img src={logo} alt="Logo"/>
+            </div>
+
+            <div className='main'>
+                <h2>The best place for you to menage your medicines!</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Main
