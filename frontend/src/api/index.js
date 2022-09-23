ef1440075a1e9cf43e67fd792af91baa70e2c9ee
Gabriel <gabrielturing@gmail.com>
2022-09-23 03:51:59 -0300

    feat: add api with axios

---
+++ frontend/src/api/index.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: '/api/v1',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  }
+});
