81d58523b3e6733b451bc8472079f6dacc1bb97c
Viniciusslara <vinicius.lara2017@hotmail.com>
2022-10-26 16:18:03 -0300

    #6 chore: add route to register medicine page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Main from './pages/Main/index'
 import Login from './pages/Login/index';
 import Register from './pages/Register/index'
 import Refactor from './pages/RefactoryPassword/index';
+import RegisterMedicine from './pages/RegisterMedicine';
 
 function App() {
   document.title = 'Medicine Manager'
@@ -15,6 +16,7 @@ function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/refactorpassword' element={<Refactor />} />
+          <Route path='/registermedicine' element={<RegisterMedicine />} />
         </Routes>
       </BrowserRouter>
     </div>
