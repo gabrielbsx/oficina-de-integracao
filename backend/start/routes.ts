6805d1dde19cd36f2e02429a1c7fbd2ede17d5a3
Gabriel <gabrielturing@gmail.com>
2022-10-28 09:40:46 -0300

    feat: add route medicine to main route #6

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -2,6 +2,7 @@ import Route from '@ioc:Adonis/Core/Route'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import './routes/users'
 import './routes/auth'
+import './routes/medicines'
 
 Route.get('/', async ({ response }: HttpContextContract) => {
   return response.send('Projeto de Oficina de Integração 2 - API')
