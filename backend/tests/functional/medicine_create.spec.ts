e885e49e70544f2e6c5999b14bf90254853bb790
Gabriel <gabrielturing@gmail.com>
2022-10-28 20:14:22 -0300

    test: create schedule for medicine #6

---
+++ backend/tests/functional/medicine_create.spec.ts
@@ -0,0 +1,41 @@
+import { test } from '@japa/runner'
+import Database from '@ioc:Adonis/Lucid/Database'
+import Cliente from 'App/Models/Cliente'
+
+test.group('MedicinesController Create', (group) => {
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
+  })
+  test('should returns a statusCode 200 if medicine is created on management medicine schedule', async ({
+    client,
+    route,
+  }) => {
+    const cpf = '123.123.123-12'
+    const password = 'any_senha'
+    await Cliente.query().where('cpf', cpf).delete()
+    await Cliente.create({
+      nome: 'any_nome',
+      email: 'any_email@mail.com',
+      cpf,
+      password: password,
+      dataNascimento: new Date(),
+    })
+    const responseAuth = await client.post(route('signin')).json({
+      cpf,
+      senha: password,
+    })
+    const { body } = responseAuth.body()
+    const medicineRow = await Database.from('medicamentos').first()
+    const medicine = {
+      idMedicamento: medicineRow.id,
+      horaGerenciamento: '10:20',
+    }
+    const bearer = `Bearer ${body.cliente.token}`
+    const responseMedCreate = await client
+      .post(route('medicines/create'))
+      .header('Authorization', bearer)
+      .json(medicine)
+    responseMedCreate.assertStatus(201)
+  })
+})
