ecdd0ad8593ebbf8b7d1064e0d7ee0d6fd45bc08
Gabriel <gabrielturing@gmail.com>
2022-10-28 17:46:03 -0300

    fix: authenticate barear to api service

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,9 +1,16 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: '/api/v1',
+const api = axios.create({
+  baseURL: "/api/v1",
   headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-  }
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
 });
+
+api.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  return config;
+});
+
+export default api;
