e167ec4dc352fd0366af4132679ac85039b4b431
Gabriel <gabrielturing@gmail.com>
2022-11-13 19:06:49 -0300

    fix: auto convert datetime database with lucid orm #6

--- backend/app/Controllers/Http/MedicinesController.ts
+++ backend/app/Controllers/Http/MedicinesController.ts
@@ -10,10 +10,10 @@ export default class MedicinesController {
     const { idMedicamento, horaGerenciamento } = request.only([
       'idMedicamento',
       'horaGerenciamento',
-    ]) as { idMedicamento: number; horaGerenciamento: Date }
+    ]) as { idMedicamento: number; horaGerenciamento: DateTime }
     const gerenciamento = new Gerenciamento()
     gerenciamento.idMedicamento = idMedicamento
-    gerenciamento.horaGerenciamento = DateTime.fromJSDate(horaGerenciamento)
+    gerenciamento.horaGerenciamento = horaGerenciamento
     gerenciamento.idCliente = await auth.use('api').user!.id
     await gerenciamento.save()
     return response.created({
