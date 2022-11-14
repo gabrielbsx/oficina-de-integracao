3f7b5d8f859f64daca908fcea3ae754738e1c572
Gabriel <gabrielturing@gmail.com>
2022-11-13 22:45:06 -0300

    test: ensure that returns a error when management medicine is not exists or invalid #9

--- backend/tests/functional/medicine_delete.spec.ts
+++ backend/tests/functional/medicine_delete.spec.ts
@@ -43,6 +43,40 @@ test.group('MedicinesController Create', (group) => {
     await cliente.delete()
     await management.delete()
   })
+  test('should returns a statusCode 404 and a message error if management medicine is not exists', async ({
+    client,
+    route,
+  }) => {
+    const medicine = await Medicamento.query().firstOrFail()
+    const cliente = new Cliente()
+    const password = 'any_senha'
+    cliente.nome = 'any_nome'
+    cliente.email = 'any_email_rand@mail.com'
+    cliente.cpf = '123.123.123-18'
+    cliente.password = password
+    cliente.dataNascimento = new Date()
+    await cliente.save()
+    const management = new Gerenciamento()
+    management.horaGerenciamento = DateTime.now()
+    management.idMedicamento = medicine.id
+    management.idCliente = cliente.id
+    await management.save()
+    const responseAuth = await client.post(route('signin')).json({
+      cpf: cliente.cpf,
+      senha: password,
+    })
+    const { body } = responseAuth.body()
+    const bearer = `Bearer ${body.cliente.token}`
+    const response = await client
+      .delete(route('medicines/delete', { id: 1000000000000000000000 }))
+      .header('Authorization', bearer)
+    response.assertStatus(404)
+    response.assertBodyContains({
+      message: 'E_ROW_NOT_FOUND: Row not found',
+    })
+    await cliente.delete()
+    await management.delete()
+  })
   test('should returns a statusCode 200 if medicine is deleted', async ({ client, route }) => {
     const medicine = await Medicamento.query().firstOrFail()
     const cliente = new Cliente()
